077552d699bbec24b7fe62a3b6cdad6db9c3d6bf
zelarin2 <jose.policarpo.oliveira@gmail.com>
2025-07-30 21:08:31 +0100

    implement submit logic

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,6 +16,7 @@ import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import Image from 'next/image'
 import Link from 'next/link'
+import { createAccount } from '@/lib/actions/user.actions'
 
 type FormType = 'sign-in' | 'sign-up'
 
@@ -32,6 +33,7 @@ const authFormSchema = (formType: FormType) => {
 const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading, setIsLoading] = useState(false)
   const [errorMessage, setErrorMessage] = useState<string>('')
+  const [accountId, setAccountId] = useState<string | null>(null)
 
   const formSchema = authFormSchema(type)
 
@@ -43,11 +45,21 @@ const AuthForm = ({ type }: { type: FormType }) => {
     },
   })
 
-  // 2. Define a submit handler.
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+    setIsLoading(true)
+
+    try {
+      const user = await createAccount({
+        fullName: values.fullName || '',
+        email: values.email,
+      })
+
+      setAccountId(user.accountId)
+    } catch {
+      setErrorMessage('Failed to create account. Please try again.')
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   return (
